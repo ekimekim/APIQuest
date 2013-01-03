aac9aebc77a1b17b020b05b4c05060670cc58fa9
Mike Lang <mike.lang@orionvm.com.au>
2013-01-03 23:20:52 +1100

    Move almost all the current code to Character, start to build tools.
    Signed-off-by: Mike Lang <mike.lang@orionvm.com.au>

--- character.py
+++ character.py
@@ -1,29 +1,84 @@
-from flask import request
+from flask import request, abort
 from app import app
-
+from simplejson import dumps
 
 chars = {}
 
 @app.route('/char/<charname>', methods=['POST'])
 def create(charname):
 	if charname in chars:
-		return "Character already exists", 409
+		return "Character already exists\n", 409
 	chars[charname] = Character(charname)
-	return 'Character created', 201
+	return 'Character created\n', 201
 
 
 @app.route('/char/<charname>')
 def get(charname):
 	if charname not in chars:
-		return "Character not found", 404
+		return "Character not found\n", 404
 	char = chars[charname]
-	return char.to_json()
+	return char.to_json() + '\n'
 
 
-@app.route('/char/<charname>/<attr>')
+class Character(object):
+	attrs = set(['name', 'position', 'x', 'y']) # Names that are externally readable
+	ops = {
+		'GET': set([]),
+		'POST': set(['move']),
+	} # Names that are externally callable
+
+	def __init__(self, name):
+		self.name = name
+		self.position = [0, 0]
+
+	x = property(lambda self: self.position[0])
+	y = property(lambda self: self.position[1])
+
+	@x.setter
+	def x(self, value):
+		self.position[0] = value
+	@y.setter
+	def y(self, value):
+		self.position[1] = value
+
+	def to_json(self):
+		return dumps(dict((attr, getattr(self, attr)) for attr in self.attrs), indent=4)
+
+	def _move(self, x, y):
+		"""Does the actual move, including any checks for whether its allowed."""
+		self.x += x
+		self.y += y
+
+	# Operations
+
+	def move(self, direction):
+		DIRECTIONS = dict(
+			northeast=(-1, 1), north=( 0, 1), northwest=( 1, 1),
+			     east=(-1, 0),                     west=( 1, 0),
+			southeast=(-1,-1), south=( 0,-1), southwest=( 1,-1))
+		SHORTCUTS = dict(ne='northeast', n='north', nw='northwest',
+		                  e='east'     ,             w='west',
+		                 se='southeast', s='south', sw='southwest')
+		direction = direction.lower()
+		if direction in SHORTCUTS: direction = SHORTCUTS[direction]
+		if direction not in DIRECTIONS:
+			return "Bad direction. Direction must be one of:\n%s" % \
+				'\n'.join(DIRECTIONS.keys() + SHORTCUTS.keys()), 400
+		direction = DIRECTIONS[direction]
+		return self._move(*direction)
+
+
+@app.route('/char/<charname>/<attr>', methods=Character.ops.keys())
 def charattr(charname, attr):
-	if attr in attrs + ops:
+	if attr in Character.attrs + Character.ops[request.method]:
+		if charname not in chars:
+			return "Character not found\n", 404
 		ret = getattr(chars[charname], attr)
-	if attr in ops:
-		ret = ret()
+	else:
+		abort(404)
+	if attr in Character.ops[request.method]:
+		try:
+			ret = ret(**request.form)
+		except TypeError:
+			abort(400)
 	return dumps(ret)
--- main.py
+++ main.py
@@ -1,26 +1,7 @@
 from flask import request, g
-from collections import namedtuple
-from simplejson import dumps
 
 from app import app
-
-chars = {}
-
-@app.route("/char/<charname>/move", methods=['POST'])
-def movechar(charname):
-	if charname not in chars:
-		return "Character not found", 404
-	char = chars[charname]
-	direction = request.form['direction']
-	if direction not in ['north', 'south', 'east', 'west', 'n', 's', 'e', 'w']:
-		return "Bad direction.", 400
-	direction = direction[0]
-	if direction == 'n': char.position[1] += 1
-	if direction == 's': char.position[1] -= 1
-	if direction == 'e': char.position[0] += 1
-	if direction == 'w': char.position[0] -= 1
-	return 'Character moved'
-
+import character
 
 if __name__=='__main__':
 	app.run(host='0.0.0.0', debug=True)
